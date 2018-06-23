23e6c242c1c6d3c5f0a64f7de1a62387fa9fb01a
mircea <mircea@neodonis.com>
2018-06-23 00:23:52 -0400

    Implement test for GET /api/owners/{ownerId}

--- urn.rest-test.org.netkernel.demo.petclinic/src/test/java/org/netkernel/demo/petclinic/RetrieveOwnerFromThePetClinicTest.java
+++ urn.rest-test.org.netkernel.demo.petclinic/src/test/java/org/netkernel/demo/petclinic/RetrieveOwnerFromThePetClinicTest.java
@@ -9,11 +9,11 @@ import org.netkernel.demo.petclinic.model.Owner;
 import org.netkernel.demo.petclinic.steps.PetClinicSteps;
 
 import static net.serenitybdd.rest.SerenityRest.rest;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
+
 
 @RunWith(SerenityRunner.class)
 public class RetrieveOwnerFromThePetClinicTest extends RestApiBase {
-    //private static final String SWAGGER_JSON_URL = "http://localhost:9966/petclinic/v2/api-docs";
     private static final String SCHEMA_ROOT_PATH = "C:/data/nk-workspace/netkernel-petclinic";
     private static final String SWAGGER_JSON_URL = "file:///" + SCHEMA_ROOT_PATH + "/urn.rest-test.org.netkernel.demo.petclinic/src/test/resources/schema/model-schema.json";
     private final SwaggerValidationFilter validationFilter = new SwaggerValidationFilter(SWAGGER_JSON_URL);
@@ -31,12 +31,16 @@ public class RetrieveOwnerFromThePetClinicTest extends RestApiBase {
     public void shouldFindSingleOwnerWithPet() {
         rest().filter(validationFilter)
                 .log().all()
-                .get("/api/owners/{ownerId}", 3)
+                .get("/api/owners/{ownerId}", 10)
                 .then()
                 .log().all()
                 .statusCode(200)
+                .and().body("id", equalTo(10))
                 .and().body("firstName", equalTo("Carlos"))
-                .and().body("lastName", equalTo("Estaban"));
+                .and().body("lastName", equalTo("Estaban"))
+                .and().body("pets.size", equalTo(2))
+                .and().body("pets.findAll {it.type.name == 'cat'}.name", hasSize(1))
+                .and().body("pets.findAll {it.type.name == 'cat'}.name", hasItem("Sly"));
     }
 
 }
