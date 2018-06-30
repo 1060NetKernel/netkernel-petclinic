5ca3838fd12f0f5cc4063d5448c753fc7b630f95
mircea <mircea@neodonis.com>
2018-06-29 20:30:03 -0400

    Start developing the NK REST API and validating against pre-built REST API automated tests

--- urn.rest-test.org.netkernel.demo.petclinic/src/test/java/org/netkernel/demo/petclinic/RestApiBase.java
+++ urn.rest-test.org.netkernel.demo.petclinic/src/test/java/org/netkernel/demo/petclinic/RestApiBase.java
@@ -9,6 +9,8 @@ import static net.serenitybdd.rest.SerenityRest.rest;
 public class RestApiBase {
     protected static final String SCHEMA_ROOT_PATH = "C:/data/nk-workspace/netkernel-petclinic";
     protected static final String SWAGGER_JSON_URL = "file:///" + SCHEMA_ROOT_PATH + "/urn.rest-test.org.netkernel.demo.petclinic/src/test/resources/schema/model-schema.json";
+    private static int SPRING_REST_API_PORT = 9966;
+    private static int NK_REST_API_PORT = 8080;
     protected final SwaggerValidationFilter validationFilter = new SwaggerValidationFilter(SWAGGER_JSON_URL);
 
     @BeforeClass
@@ -16,7 +18,11 @@ public class RestApiBase {
         resetDatabase();
 
         RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 9966;
+        //NOTE: All REST API tests have been developed and validated against existing Spring REST API impl.
+        //RestAssured.port = SPRING_REST_API_PORT;
+        //NOTE: While implementing the REST API with NK we target tests to NK REST API endpoint. NK impl will pass the
+        //existing REST API characterizing tests one test at a time. Once NK impl passes all the tests then we are done.
+        RestAssured.port = NK_REST_API_PORT;
         RestAssured.basePath = "/petclinic";
     }
 
